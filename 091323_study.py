c3f78aafbbd218304cd64eea64b491c3f257a27c
eric <whitecanvas24@gmail.com>
2023-10-16 20:51:45 +0900

    first commit

---
+++ 091323_study.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+food = ["Pizza", "KFC", "Coke"]
+
+def order():
+    if(x.get() == 0):
+        print("You picked a Pizza.")
+    elif(x.get() == 1):
+        print("You picked KFC.")
+    elif(x.get() == 2):
+        print("You picked Cocacola.")
+    else:
+        print("Sorry?")
+
+
+window = Tk()
+window.title("Let's eat 여보!")
+
+PizzaImage = PhotoImage(file = "D:\Coding study\Python\Beginner Projects\pizza.png")
+KFCImage = PhotoImage(file = "D:\Coding study\Python\Beginner Projects\kfc.png")
+CokeImage = PhotoImage(file = "D:\Coding study\Python\Beginner Projects\cocacola.png")
+foodImages = [PizzaImage, KFCImage, CokeImage]
+
+x = IntVar()
+
+for index in range(len(food)):
+    radiobutton = Radiobutton(window,
+                              text=food[index],
+                              variable=x,
+                              value=index,
+                              padx=25,
+                              font=("Impact", 25),
+                              image=foodImages[index],
+                              compound=LEFT,
+                              indicatoron=0,
+                              width=700,
+                              command=order)
+    
+    radiobutton.pack()
+
+window.mainloop()
